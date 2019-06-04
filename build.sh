8408ce77b68e7e431d6ac0ff479249c0b0b84b02
Theo Sun <theo.sun@sap.com>
2019-06-04 18:30:07 +0800

    fix(ci): script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # CI build script
 
-
-# install marp clie
-npm i -g @marp-team/marp-cli
-
 # generate pdf
-for f in `find . -type f -name "*.md"`
-do marp $f --pdf -o "./dist/$f.pdf" ; done
+for f in `find ./src -type f -name "*.md"`
+do ./node_modules/.bin/marp $f --pdf -o "./dist/${f:5}.pdf" ; done
+
+# generate html
+for f in `find ./src -type f -name "*.md"`
+do ./node_modules/.bin/marp $f --html -o "./dist/${f:5}.html" ; done
