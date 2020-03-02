18c12853b1cb30aa6765be1c7b323b4587f112b7
Theo Sun <theo.sun@sap.com>
2020-03-02 15:59:36 +0800

    fix: build

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # CI build script
 
+export CHROME_PATH=${pwd}/node_modules/puppeteer/.local-chromium/linux-722234
+
 # generate pdf
 for f in `find ./src -type f -name "*.md"`
 do fb="${f%.*}"; ./node_modules/.bin/marp $f --pdf -o "./dist/${fb:5}.pdf" ; done
