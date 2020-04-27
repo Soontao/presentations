8fbda861157ae111e722bf0e9d5e2bfe31cb6c36
Theo Sun <theo.sun@sap.com>
2020-04-27 14:24:21 +0800

    chore: add tests

---
+++ src/node/cim-training/session-5-example/debug/mocha/app/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.get("/add", req => {
+  const { v1, v2 } = req.body
+  req.res.json({ result: v1 + v2 })
+})
+
+if (require.main == module) {
+  app.listen(3000, () => { console.log("app started") })
+}
+
+module.exports = app
---
+++ src/node/cim-training/session-5-example/debug/mocha/test/server.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const fetch = require("node-fetch")
+const app = require("../app/server")
+const process = require("process")
+
+
+describe('server', function () {
+
+  const port = process.env.PORT || "3000"
+
+  before(function (done) {
+    app.listen(parseInt(port, 10), done)
+  });
+
+  describe('/add', function () {
+    it('should return the sum', async function () {
+      const response = await fetch(`http://127.0.0.1:${port}/add`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ v1: 123, v2: 321 })
+        }
+      )
+      const body = await response.json()
+      assert.equal(body.result, 444)
+    });
+  });
+});
