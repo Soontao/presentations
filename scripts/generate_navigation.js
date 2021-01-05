154fe903b1c72398c178f6be7316e3a03ab572f1
Theo Sun <theo.sun@sap.com>
2021-01-05 13:15:28 +0800

    chore: adjust index page

--- scripts/generate_navigation.js
+++ scripts/generate_navigation.js
@@ -3,7 +3,6 @@ const fs = require("fs").promises;
 const { parse } = require("@textlint/markdown-to-ast");
 const { get, find } = require("@newdash/newdash-node");
 const path = require("path");
-const pretty = require("pretty")
 
 const findTitleForMarkdown = (content) => {
   const ast = parse(content);
@@ -17,61 +16,6 @@ const formatPresentationName = (s = "") => {
   return `${s.substring(4, s.length - 3)}.html`
 };
 
-
-const generateIndexHtml = (f) => `
-<!DOCTYPE html>
-<html>
-
-<head>
-  <title>Presentation List</title>
-  <script 
-    id="sap-ui-bootstrap" 
-    src="https://openui5.hana.ondemand.com/resources/sap-ui-core.js"
-    data-sap-ui-theme="sap_fiori_3" 
-    data-sap-ui-libs="sap.m" 
-    data-sap-ui-resourceroots='{"index": "./"}'
-    data-sap-ui-async="true"
-    displayBlock="true"
-    data-sap-ui-xx-waitfortheme="true"
-  >
-  </script>
-</head>
-
-<body class="sapUiBody" id="content">
-</body>
-
-<script>
-  sap.ui.getCore().attachInit(function () {
-    const navData = ${JSON.stringify(f, undefined, 2)}
-    const list = new sap.m.SelectList({
-      items: {
-        path: "/",
-        template: new sap.ui.core.ListItem({
-          text: "{title}"
-        })
-      },
-      itemPress: event => {
-        const presentationPath = event.getParameter("item").getBindingContext().getObject("path")
-        window.open(presentationPath)
-      }
-    })
-
-    list.setModel(new sap.ui.model.json.JSONModel(navData))
-
-    const app = new sap.m.App({
-      pages: new sap.m.Page({
-        title: "Presentation List",
-        content: [list]
-      })
-    })
-
-    app.placeAt("content")
-  });
-</script>
-
-</html>
-`
-
 const readFile = file => fs.readFile(file, { encoding: "UTF-8" });
 
 (
@@ -84,8 +28,8 @@ const readFile = file => fs.readFile(file, { encoding: "UTF-8" });
     }))
 
     await fs.writeFile(
-      path.join(__dirname, "../dist/index.html"),
-      pretty(generateIndexHtml(presentations)),
+      path.join(__dirname, "../dist/presentations.json"),
+      JSON.stringify(presentations, undefined, 2),
       { encoding: "UTF-8" }
     )
 
