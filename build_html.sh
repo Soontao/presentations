876d0bcc56094fb80f7b5b0d166db4193b65b946
Theo Sun <theo.sun@outlook.com>
2021-06-19 14:47:59 +0800

    chore(ci): script update

--- build_html.sh
+++ build_html.sh
@@ -3,7 +3,7 @@
 
 # generate html
 for f in `find ./src -type f -not -path "*node_modules*" -not -path "*example*" -name "*.md"`
-do fb="${f%.*}"; ./node_modules/.bin/marp $f --html  --theme ./theme/dark.css -o "./dist/${fb:5}.html" ; done
+do fb="${f%.*}"; ./node_modules/.bin/marp $f --html -o "./dist/${fb:5}.html" ; done
 
 # build index
 node scripts/generate_navigation.js
