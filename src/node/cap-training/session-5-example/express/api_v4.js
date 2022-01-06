0226e8620004a08a49d644a99b07ded0c46b2275
Theo Sun <theo.sun@outlook.com>
2022-01-06 20:20:19 +0800

    chore: enhance express example

--- src/node/cap-training/session-5-example/express/api_v4.js
+++ src/node/cap-training/session-5-example/express/api_v4.js
@@ -1,6 +1,14 @@
 const { Router } = require("express")
+const req = require("express/lib/request")
 
 const api_v4 = Router()
+
+// http://localhost:3000/api/v4/health
+api_v4.get("/health", (req) => {
+    req.res.json({
+        "status": 200
+    })
+})
 
 // http://localhost:3000/api/v4/throw-error-cb
 api_v4.get("/throw-error-cb", (req) => {
@@ -47,9 +55,24 @@ api_v4.get("/forget-response-correct", (req) => {
     }
 })
 
+// http://localhost:3000/api/v4/crash-1
+api_v4.get("/crash-1", () => {
+    setTimeout(() => { throw new Error() })
+})
+
+// http://localhost:3000/api/v4/crash-2
+api_v4.get("/crash-2", () => {
+    new Promise((resolve, reject) => reject(new Error()))
+})
+
 // http://localhost:3000/api/v4/block-js
 api_v4.get("/block-js", req => {
-    for (; ;) { } // js engine will stop at here forever
+    let i = 1
+    for (let index = 0; index < 5000000000; index++) {
+        i + index
+    }
+    console.log("finished")
+    req.res.status(200).send()
 })
 
 module.exports = api_v4
