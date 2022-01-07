53fe66f6713077ff0e0cc903a117ee3ff34b5597
Theo Sun <theo.sun@sap.com>
2022-01-07 12:12:00 +0800

    chore: update

--- src/node/cap-training/session-6-example/cap-01/srv/lib/class-service.js
+++ src/node/cap-training/session-6-example/cap-01/srv/lib/class-service.js
@@ -1,9 +1,19 @@
 
+
+const logger = cds.log("class-service")
 /**
  * @param {import("@sap/cds/apis/services").Service} srv
  */
 module.exports = (srv) => {
+
   srv.on("READ", "Classes", async (req, next) => {
+    logger.info("read classes handler 1")
+    // do something
+    return await next()
+  })
+
+  srv.on("READ", "Classes", async (req, next) => {
+    logger.info("read classes handler 2")
     // do something
     return await next()
   })
--- src/node/cap-training/session-6-example/cap-01/srv/server.js
+++ src/node/cap-training/session-6-example/cap-01/srv/server.js
@@ -1,5 +1,21 @@
 const cds = require('@sap/cds')
 
-cds.on('bootstrap', app => app.use(require('cds-swagger-ui-express')()))
+cds.on('bootstrap', app => {
+  app.use(require('cds-swagger-ui-express')())
+
+})
+
+// an exmaple for global event hook
+cds.on('serving', service => {
+  if (service instanceof cds.ApplicationService) {
+    const logger = cds.log(service.name)
+    service.prepend('*', srv => srv.on('*', async (evt, next) => {
+      logger.info(`before ${evt.event}`)
+      const rt = await next()
+      logger.info(`after ${evt.event}`)
+      return rt;
+    }))
+  }
+})
 
 module.exports = cds.server
